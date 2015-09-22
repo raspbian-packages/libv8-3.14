108cef85e6b5b98ce1ab9a8f211f67fff4446502
Debian Javascript Maintainers <pkg-javascript-devel@lists.alioth.debian.org>
2015-09-22 01:07:37 +0000

    _kfreebsd
    Gbp-Pq: Name 0001_kfreebsd.patch

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -28,7 +28,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import multiprocessing
+import multiprocessing.dummy as multiprocessing
 import optparse
 import os
 from os.path import join
