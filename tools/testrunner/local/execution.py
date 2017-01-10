f24c3397c668e368d14a355854ff5e5ca5ba44e3
Debian Javascript Maintainers <pkg-javascript-devel@lists.alioth.debian.org>
2017-01-10 14:26:12 +0000

    _kfreebsd
    Gbp-Pq: Name 0001_kfreebsd.patch

--- tools/testrunner/local/execution.py
+++ tools/testrunner/local/execution.py
@@ -26,7 +26,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import multiprocessing
+import multiprocessing.dummy as multiprocessing
 import os
 import threading
 import time
